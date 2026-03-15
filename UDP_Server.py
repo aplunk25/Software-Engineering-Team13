467a313261d6222bf980c530fe5fb2f21551cf5d
Jortiz2003 <118001397+Jortiz2003@users.noreply.github.com>
2026-03-15 03:23:06 -0500

    Update UDP server to support broadcasting
    Enable broadcasting and modify socket behavior.

--- UDP_Server.py
+++ UDP_Server.py
@@ -1,56 +1,122 @@
 # UDP Server Script to listen for incoming UPD packets.
 
+
+
 # Problems: What confirmation message should the server send? Probably the same message it received or the equipment code.
+
+
 
 import socket
 
+
 localIP = "0.0.0.0"
+
 localPort = 7501
+
 broadcastPort = 7500
+
 bufferSize = 1024
+
 msgFromServer = None
+
 # bytesToSend = str.encode(msgFromServer)
 
 # Create a datagram socket for localPort
+
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Create a datagram socket for broadcastPort
+
 UDPBroadcastSocket = socket.socket(
+
     family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
     
 # Allow immediate reuse of address
+
 UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 UDPBroadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+
+
+# Enable broadcast (only once, not inside the loop)
+
+UDPBroadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+
+
 # Bind to address and ip
+
 UDPServerSocket.bind((localIP, localPort))
 
+
+
 # Bind address to broadcast port
-UDPBroadcastSocket.bind((localIP, broadcastPort))
+
+#UDPBroadcastSocket.bind((localIP, broadcastPort))
+
+
+
 
 
 print("UDP server up and listening")
 
+
+
 # Listen for incoming datagrams
+
+
 
 while (True):
 
+
+
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
     message_bytes = bytesAddressPair[0]
+
     address = bytesAddressPair[1]
+
     
+
     # Decode bytes to string
+
     message = message_bytes.decode()
+
     
+
     clientMsg = "Client Message: {}".format(message)
+
     clientIP = "Client IP Address: {}".format(address)
 
+
+
     print(clientMsg)
+
     print(clientIP)
 
+
+
     # Prepare reply message
+
     msgFromServer = clientMsg
+
     bytesToSend = str.encode(msgFromServer)
 
+
+
     # Sending a reply to client
-    UDPBroadcastSocket.sendto(bytesToSend, address)
+
+    UDPServerSocket.sendto(bytesToSend, address) #replace second part with address for UDP_client testing
+
+    
+
+    # Also broadcast to traffic generator (or anyone listening on 7500)
+
+    UDPBroadcastSocket.sendto(message_bytes, ('127.0.0.1', broadcastPort)) #message_bytes to send exactly what the client sent
+
+	
+
+	
+

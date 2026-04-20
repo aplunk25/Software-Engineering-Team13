a39492e8c8d6b5e651ccfe6c71ee2a15f5996d7e
Miles Schluterman <mhs017@uark.edu>
2026-04-19 23:36:43 -0500

    Update countdown to pass players to launch_play_action
    Modified countdown completion behavior to include players.

--- player_entry.py
+++ player_entry.py
@@ -473,8 +473,8 @@ class EntryTerminal:
         # Start countdown; when it finishes, launch the Play Action display
         def _after_countdown():
             # Close player entry screen
-            # `self.root.destroy()`
-            launch_play_action(self.root, self.pg_config)
+            self.root.destroy()
+            launch_play_action(self.root, self.pg_config, players)
 
         CountdownTimer(self.root, on_close=_after_countdown,
                        image_path="countdown_images", seconds=30)
